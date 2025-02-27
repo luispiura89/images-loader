71d6690e76b59e7b7a6f3bd4b707fe1c38714d8c
Luis Piura <luisf.piura@hotmail.com>
2025-02-27 16:30:56 -0600

    project creation

---
+++ image-loader/image_loaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_loaderApp.swift
+//  image-loader
+//
+//  Created by Luis Francisco Piura Mejia on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct image_loaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ image-loaderUITests/image_loaderUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  image_loaderUITestsLaunchTests.swift
+//  image-loaderUITests
+//
+//  Created by Luis Francisco Piura Mejia on 27/2/25.
+//
+
+import XCTest
+
+final class image_loaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
